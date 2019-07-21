2ea1f8c876aa30d4f6ae17b32c74d4acc87e6e1a
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2019-07-20 20:22:29 -0600

    upgrade to v5

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-// --------------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------------------------------- //
 
 // Simple Git GUI - A simple Git GUI, free and open
-// Copyright (C) 2017-2018  Hugo Xu
+// Copyright (C) 2017-2019  Hugo Xu
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -16,21 +16,25 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-// --------------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------------------------------- //
 
 // Main process entry point
 
-// --------------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------------------------------- //
 
 "use strict";
 
-// --------------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------------------------------- //
 
-const { app, BrowserWindow: win, ipcMain: ipc, dialog, shell } = require("electron");
+const DEBUG = false;
+
+// ----------------------------------------------------------------------------------------------------------------- //
+
+const { app, BrowserWindow: win, ipcMain: ipc, shell } = require("electron");
 const path = require("path");
 const url = require("url");
 
-// --------------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------------------------------- //
 
 ipc.on("console log", (e, data) => {
     console.log(data.log);
@@ -44,7 +48,7 @@ ipc.on("get home", (e) => {
     e.returnValue = app.getPath("home");
 });
 
-// --------------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------------------------------- //
 
 ipc.on("open project page", (e) => {
     shell.openExternal("https://github.com/jspenguin2017/SimpleGitGUI");
@@ -61,32 +65,71 @@ ipc.on("show file in folder", (e, data) => {
     e.sender.send("show file in folder done");
 });
 
-// --------------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------------------------------- //
 
 let main;
 
-app.on("ready", () => {
-    main = new win({
-        width: 1300,
-        height: 700,
-        minHeight: 550,
-        minWidth: 1250
+if (app.requestSingleInstanceLock()) {
+    app.on("ready", () => {
+        main = new win({
+            height: 700,
+            minHeight: 550,
+            minWidth: 1250,
+            show: false,
+            width: 1300,
+            webPreferences: {
+                backgroundThrottling: false,
+                nodeIntegration: true,
+
+                // TODO: Is this needed?
+                contextIsolation: false,
+            },
+        });
+
+        main.setMenu(null);
+
+        main.loadURL(url.format({
+            pathname: path.join(__dirname, "index.html"),
+            protocol: "file:",
+            slashes: true,
+        }));
+
+        main.once("ready-to-show", () => {
+            main.show();
+        });
+
+        main.webContents.on("new-window", (e) => {
+            e.preventDefault();
+            console.warn("WARN: New window blocked, there might be a security bug in the renderer process!");
+        });
+
+        main.webContents.on("will-navigate", (e) => {
+            e.preventDefault();
+            console.warn("WARN: Navigation blocked, there might be a security bug in the renderer process!");
+        });
+
+        if (DEBUG)
+            main.webContents.openDevTools();
     });
 
-    main.setMenu(null);
-
-    main.loadURL(url.format({
-        pathname: path.join(__dirname, "index.html"),
-        protocol: "file:",
-        slashes: true
-    }));
-
-    main.on("closed", () => {
-        app.quit();
+    app.on("second-instance", () => {
+        if (main)
+            main.Focus();
     });
 
-    // Debug only, uncomment as needed
-    //main.webContents.openDevTools();
-});
+    const remoteBlocker = (e) => {
+        e.preventDefault();
+        console.warn("WARN: Remote request blocked, there might be a security bug in the renderer process!");
+    };
 
-// --------------------------------------------------------------------------------------------- //
+    app.on("remote-get-builtin", remoteBlocker);
+    app.on("remote-get-current-web-contents", remoteBlocker);
+    app.on("remote-get-current-window", remoteBlocker);
+    app.on("remote-get-global", remoteBlocker);
+    app.on("remote-get-guest-web-contents", remoteBlocker);
+    app.on("remote-require", remoteBlocker);
+} else {
+    app.quit();
+}
+
+// ----------------------------------------------------------------------------------------------------------------- //
